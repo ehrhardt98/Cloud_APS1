b74f81cce20852c2975a491cf70f67bc2941948e
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-11-19 14:05:28 -0300

    Replacesd with UserData argument for create_instances

--- install.sh
+++
@@ -1,8 +0,0 @@
-sudo apt update
-sudo apt install -y python3-pip
-pip3 install fastapi
-pip3 install pydantic==0.32.2
-echo "export PATH=\"$/PATH:/home/ubuntu/.local/bin/\"" >> ~/.bashrc
-source ~/.bashrc
-pip3 install uvicorn
-uvicorn main:app --reload

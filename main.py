400845d585893f90291c443ae36157f119a07d4e
ehrhardt98 <jorgeavbe@al.insper.edu.br>
2019-11-24 16:22:38 -0300

    testing mongoDB CR

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+import pymongo
+import os
+
+ip = os.getenv("mongodb_ip")
+
+addr = "mongodb://" + ip + ":27017/"
+mongo_client = pymongo.MongoClient(addr)
+db = mongo_client['cloud_database']
+tasks = db['tasks']
 
 app = FastAPI()
-
-dict_tasks = dict()
 
 class Tasks(BaseModel):
     name: str
@@ -15,38 +22,46 @@ async def read_root():
 
 @app.get("/task")
 async def list_tasks():
-    return dict_tasks
+    ret = []
+    for i in tasks.find(): # .sort( {'priority': 1} ):
+        ret.append(
+            {
+                'id': i["_id"], 
+                'name': i["name"], 
+                'priority': i["priority"]
+            }
+        )
+    return ret
 
 @app.post("/task")
 async def add_task(task: Tasks):
+    ret = {
+        'name': task.name,
+        'priority': task.priority,
+    }
+    tasks.insert(ret)
 
-    if len(dict_tasks.keys()) > 0:
-        dict_tasks[max(dict_tasks.keys()) + 1] = (task.name, task.priority)
-        return {"new task": task.name, "priority": task.priority}
-    else:
-        dict_tasks[0] = (task.name, task.priority)
-        return {"new task": task.name, "priority": task.priority}
 
-@app.get("/task/{id}")
-async def get_task_by_id(id: int):
-    return dict_tasks.get(id)
+# @app.get("/task/{id}")
+# async def get_task_by_id(id: int):
+#     return dict_tasks.get(id)
 
-@app.put("/task/{id}")
-async def update_task_by_id(id: int, task: Tasks):
-    if id in dict_tasks.keys():
-        dict_tasks[id] = (task.name, task.priority)
-        return (task.name, task.priority)
-    else:
-        raise HTTPException(status_code=404, detail="no task with id {}".format(id))
+# @app.put("/task/{id}")
+# async def update_task_by_id(id: int, task: Tasks):
+#     if id in dict_tasks.keys():
+#         dict_tasks[id] = (task.name, task.priority)
+#         return (task.name, task.priority)
+#     else:
+#         raise HTTPException(status_code=404, detail="no task with id {}".format(id))
 
-@app.delete("/task/{id}")
-async def delete_task_by_id(id: int):
-    if id in dict_tasks.keys():
-        dict_tasks.pop(id)
-        return {"deleted task with id": id}
-    else:
-        raise HTTPException(status_code=404, detail="no task with id {}".format(id))
+# @app.delete("/task/{id}")
+# async def delete_task_by_id(id: int):
+#     if id in dict_tasks.keys():
+#         dict_tasks.pop(id)
+#         return {"deleted task with id": id}
+#     else:
+#         raise HTTPException(status_code=404, detail="no task with id {}".format(id))
 
-@app.get("/healthcheck", status_code=200)
-async def healthcheck():
-    return
+# @app.get("/healthcheck", status_code=200)
+# async def healthcheck():
+#     return
